de71cd734a9c0ce47f91669cb86eafb99669e7a6
artium <artium59@gmail.com>
2019-11-17 16:16:27 +0900

    Fix MobileNet_v2

--- MobileNet_v2.py
+++ MobileNet_v2.py
@@ -2,9 +2,7 @@
 https://arxiv.org/abs/1801.04381
 """
 
-from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.layers import (Conv2D, BatchNormalization, ReLU, Dense, Add,
-                                            DepthwiseConv2D, GlobalAveragePooling2D, Reshape)
+from tensorflow.python.keras import Input, Model, layers
 
 
 def convolution_block(inputs, filters, kernel_size, stride):
@@ -15,12 +13,13 @@ def convolution_block(inputs, filters, kernel_size, stride):
         Tensor
     """
 
-    x = Conv2D(filters=filters, kernel_size=kernel_size, strides=stride, padding='same', use_bias=False)(inputs)
-    x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
-    return ReLU(6.)(x)
+    x = layers.Conv2D(filters=filters, kernel_size=kernel_size,
+                      strides=stride, padding='same', use_bias=False)(inputs)
+    x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
+    return layers.ReLU(max_value=6.0)(x)
 
 
-def bottleneck_block(inputs, t, filters, stride, adding=False):
+def bottleneck_block(inputs, expansion, filters, stride):
     """
     # INPUT
         input: Tensor
@@ -30,36 +29,37 @@ def bottleneck_block(inputs, t, filters, stride, adding=False):
         Tensor
     """
 
-    expansion = int(t * inputs.shape[-1])
-
     #  input: h x w x k
     # output: h x w x (tk)
     # Not use first residuals
-    x = convolution_block(inputs, expansion, 1, 1) if filters != 16 else inputs
+    x = convolution_block(inputs, int(expansion * inputs.shape[-1]), 1, 1) if filters != 16 else inputs
 
     #  input: h x w x (tk)
     # output: h/s x w/s x (tk)
-    x = DepthwiseConv2D(kernel_size=3, strides=stride, padding='same', use_bias=False)(x)
-    x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
-    x = ReLU(6.)(x)
+    if stride == 2:
+        x = layers.ZeroPadding2D(padding=(1, 1))(x)
+    x = layers.DepthwiseConv2D(kernel_size=3, strides=stride,
+                               padding='same' if stride == 1 else 'valid', use_bias=False)(x)
+    x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
+    x = layers.ReLU(max_value=6.0)(x)
 
     #  input: h/s x w/s x (tk)
     # output: h/s x w/s x filters
-    x = Conv2D(filters=filters, kernel_size=1, strides=1, padding='same', use_bias=False)(x)
-    x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
+    x = layers.Conv2D(filters=filters, kernel_size=1, strides=1, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
 
-    if adding:
-        x = Add()([inputs, x])
+    if inputs.shape[-1] == x.shape[-1] and stride == 1:
+        x = layers.Add()([inputs, x])
 
     # h/s x w/s x k'
     return x
 
 
 def inverted_residuals_block(inputs, t, filters, n, stride):
-    x = bottleneck_block(inputs, t, filters, stride, False)
+    x = bottleneck_block(inputs, t, filters, stride)
 
     for _ in range(1, n):
-        x = bottleneck_block(x, t, filters, 1, True)
+        x = bottleneck_block(x, t, filters, 1)
 
     return x
 
@@ -68,7 +68,7 @@ def MobileNet_v2(input_shape, num_classes):
     inputs = Input(shape=input_shape)
     x = convolution_block(inputs, 32, 3, 2)
 
-    # input: input, t, filters, n, stride
+    # input: input, expansion(t), filters(c), repeat(n), stride(s)
     x = inverted_residuals_block(x, 1, 16, 1, 1)
     x = inverted_residuals_block(x, 6, 24, 2, 2)
     x = inverted_residuals_block(x, 6, 32, 3, 2)
@@ -78,15 +78,15 @@ def MobileNet_v2(input_shape, num_classes):
     x = inverted_residuals_block(x, 6, 320, 1, 1)
 
     x = convolution_block(x, 1280, 1, 1)
-    x = GlobalAveragePooling2D()(x)
-    x = Reshape((1, 1, 1280))(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Reshape((1, 1, 1280))(x)
 
-    # x = keras.layers.Dropout(rate=0.3)(x)
-    # x = keras.layers.Conv2D(filters=num_classes, kernel_size=1, padding='same')(x)
-    # x = keras.layers.Softmax()(x)
+    # x = layers.Dropout(rate=0.3)(x)
+    # x = layers.Conv2D(filters=num_classes, kernel_size=1, padding='same')(x)
+    # x = layers.Softmax()(x)
 
-    x = Dense(num_classes, activation='softmax', use_bias=True)(x)
-    output = Reshape((num_classes, ))(x)
+    x = layers.Dense(num_classes, activation='softmax', use_bias=True)(x)
+    output = layers.Reshape((num_classes, ))(x)
 
     # Create model
     model = Model(inputs, output)
@@ -95,7 +95,7 @@ def MobileNet_v2(input_shape, num_classes):
 
 
 if __name__ == '__main__':
-    input_shape = (48, 48, 1)
-    num_classes = 7
+    input_shape = (224, 224, 3)
+    num_classes = 1000
     model = MobileNet_v2(input_shape, num_classes)
     model.summary()
